59b4a7949dce247c46a7a90c2d17bcf5e08ac059
kenzabachsin <kenzabachsin@gmail.com>
2026-06-07 17:05:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function initCanvas() {
 function generateHeartShape() {
     heartPoints = [];
     const scale = Math.min(canvas.width, canvas.height) / 35;
-    for (let i = 0; i < 200; i++) {
-        let t = (i / 200) * Math.PI * 2;
+    for (let i = 0; i < 150; i++) {
+        let t = (i / 150) * Math.PI * 2;
         heartPoints.push({
             x: canvas.width/2 + 16*Math.pow(Math.sin(t), 3) * scale,
             y: (canvas.height/2 - 50) - (13*Math.cos(t) - 5*Math.cos(2*t) - 2*Math.cos(3*t) - Math.cos(4*t)) * scale
@@ -23,27 +23,24 @@ function generateHeartShape() {
 
 class Particle {
     constructor() {
-        this.x = canvas.width/2; this.y = canvas.height/2;
-        this.vx = (Math.random()-0.5)*15; this.vy = (Math.random()-0.5)*15;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.target = null;
     }
     update() {
         if(this.target) {
-            this.x += (this.target.x - this.x) * 0.05;
-            this.y += (this.target.y - this.y) * 0.05;
-        } else {
-            this.x += this.vx; this.y += this.vy;
-            this.vx *= 0.95; this.vy *= 0.95;
+            this.x += (this.target.x - this.x) * 0.08;
+            this.y += (this.target.y - this.y) * 0.08;
         }
     }
     draw() {
-        ctx.fillStyle = '#ff4081';
-        ctx.beginPath(); ctx.arc(this.x, this.y, 3, 0, Math.PI*2); ctx.fill();
+        ctx.fillStyle = '#FFC1CC'; // Warna soft pink
+        ctx.beginPath(); ctx.arc(this.x, this.y, 2, 0, Math.PI*2); ctx.fill();
     }
 }
 
 function startLoveParticles() {
-    for(let i=0; i<200; i++) particles.push(new Particle());
+    for(let i=0; i<150; i++) particles.push(new Particle());
     setTimeout(() => {
         particles.forEach((p, i) => p.target = heartPoints[i]);
     }, 1000);
